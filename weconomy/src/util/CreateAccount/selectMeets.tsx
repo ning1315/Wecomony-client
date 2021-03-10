b51f1c232dba019dd3da3d80efa8831a361175fa
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-03-10 21:34:16 +0900

    debug ? error

--- weconomy/src/util/CreateAccount/selectMeets.tsx
+++ weconomy/src/util/CreateAccount/selectMeets.tsx
@@ -5,34 +5,32 @@ import Select from '@material-ui/core/Select';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
 interface selects {
-    selectedMeet: any;
-    handleMeetChange:any;
+  selectedMeet: any;
+  handleMeetChange: any;
 }
 
-
-const SelectMeets:React.FC<selects> = ({selectedMeet, handleMeetChange}) => {
-
-    return (
-        <>
-        <InputLabel id="demo-simple-select-helper-label">그룹 목록</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          name="meetList"
-          value={selectedMeet || ''}
-          onChange={handleMeetChange}
-          style={{ width: '150px', textAlign: 'center' }}
-        >
-          <MenuItem value="">
-            <em>선택해주세요</em>
-          </MenuItem>
-          <MenuItem value={10}>그룹1</MenuItem>
-          <MenuItem value={20}>그룹2</MenuItem>
-          <MenuItem value={30}>그룹3</MenuItem>
-        </Select>
-        <FormHelperText>그룹을 선택해주세요</FormHelperText>
-        </>
-    )
-}
+const SelectMeets: React.FC<selects> = ({ selectedMeet, handleMeetChange }) => {
+  return (
+    <>
+      <InputLabel id="demo-simple-select-helper-label">그룹 목록</InputLabel>
+      <Select
+        labelId="demo-simple-select-helper-label"
+        id="demo-simple-select-helper"
+        name="meetList"
+        value={selectedMeet || ''}
+        onChange={handleMeetChange}
+        style={{ width: '150px', textAlign: 'center' }}
+      >
+        <MenuItem value="">
+          <em>선택해주세요</em>
+        </MenuItem>
+        <MenuItem value={10}>그룹1</MenuItem>
+        <MenuItem value={20}>그룹2</MenuItem>
+        <MenuItem value={30}>그룹3</MenuItem>
+      </Select>
+      <FormHelperText>그룹을 선택해주세요</FormHelperText>
+    </>
+  );
+};
 
 export default SelectMeets;
